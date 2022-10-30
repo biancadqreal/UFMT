0d08d909e1f7349fdc07b88c77c0f8dfe98ed462
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-10-30 19:22:19 -0300

    Create madeInPython003.py

---
+++ Programacao 1/madeInPython003.py
@@ -0,0 +1,4 @@
+name = str(input("Tell me a name: "))
+adress = str(input("Tell me a adress: "))
+number = int(input("Tell me a number: "))
+print("Your name is {}, your adress is {} and your number is {}".format(name, adress , number))

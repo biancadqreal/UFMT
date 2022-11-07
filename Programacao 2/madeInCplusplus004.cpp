e9faa7b45829306c113ebf9e21c57fa227aa241a
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-11-07 09:30:03 -0300

    Update madeInCplusplus004.cpp

--- Programacao 2/madeInCplusplus004.cpp
+++ Programacao 2/madeInCplusplus004.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int soma(int x, int y){
+    cout << "A soma de " << x << " e " << y << " é " << x+y;
+}
+
+int main(){
+    int a, b;
+    cout << "Digite o valor de a: ";
+    cin >> a;
+    
+    cout << "Digite o valor de b: ";
+    cin >> b;
+    soma(a, b);
+}

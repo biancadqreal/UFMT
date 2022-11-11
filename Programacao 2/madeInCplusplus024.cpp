9e850896a24e0d16f16563b974ff12c006d4bece
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-11-10 22:57:47 -0300

    Create madeInCplusplus024.cpp

---
+++ Programacao 2/madeInCplusplus024.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    string s;
+    cin >> s;
+    reverse(s.begin(),s.end());//Inverte string
+++    
+    cout << s << endl;
+    return 0;
+}

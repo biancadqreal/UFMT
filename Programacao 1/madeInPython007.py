cb61cf729597f67b7b193492b46d374a7a025892
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-11-28 09:30:08 -0300

    Create madeInPython007.py

---
+++ Programacao 1/madeInPython007.py
@@ -0,0 +1,13 @@
+n1 = int(input('Tell me a number: '))
+n2 = int(input('Tell me another number: '))
+
+def addition(n1, n2):
+    s = n1 + n2
+    if s > 10:
+        print(s)
+    else:
+        print(s)
+        print('The resultant value is less than 10')
+        
+addition(n1, n2)
+    

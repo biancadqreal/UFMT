1584eb9bfd8ffc898eb093a3ff9109acf61c9973
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-11-29 05:41:09 -0300

    Create madeInPython009.py

---
+++ Programacao 1/madeInPython009.py
@@ -0,0 +1,10 @@
+n1 = int(input('Tell me a number: '))
+def ismultiple(number):
+    if number%3 == 0:
+        print('Is multiple of three')
+    elif number == 0:
+        print('Is multiple of three')
+    else:
+        print('Is not a multiple of three')
+        
+ismultiple(n1)
